e411eae866cf0574732081ab5b3955ccee0a6370
mo-yusu7 <garjaw1998@gmail.com>
2023-06-15 17:55:02 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+  *main- is a entry point of the program
+  *Return: always 0 (success)
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art");
+	return (0);
+}
